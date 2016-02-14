225c2d6a85df8f822c3fa541c2aee96c0bbfb56e
Zach Klippenstein <zach.klippenstein@gmail.com>
2016-02-14 07:50:33 -0800

    Ran skynet.go through gofmt.

--- go/skynet.go
+++ go/skynet.go
@@ -4,27 +4,27 @@ import "fmt"
 import "time"
 
 func skynet(c chan int, num int, size int, div int) {
-    if (size == 1) {
-        c <- num
-    } else {
-        rc := make(chan int)
-        sum := 0
-        for i := 0; i < div; i++ {
-            sub_num := num + i * (size / div)
-            go skynet(rc, sub_num, size / div, div)
-        }
-        for i := 0; i < div; i++ {
-            sum += <-rc
-        }
-        c <- sum
-    }
+	if size == 1 {
+		c <- num
+	} else {
+		rc := make(chan int)
+		sum := 0
+		for i := 0; i < div; i++ {
+			sub_num := num + i*(size/div)
+			go skynet(rc, sub_num, size/div, div)
+		}
+		for i := 0; i < div; i++ {
+			sum += <-rc
+		}
+		c <- sum
+	}
 }
 
 func main() {
-    c := make(chan int)
-    start := time.Now().UnixNano() / 1000000 
-    go skynet(c, 0, 1000000, 10)
-    result := <-c
-    end := time.Now().UnixNano() / 1000000
-    fmt.Printf("Result: %d in %d ms.\n", result, end - start)
+	c := make(chan int)
+	start := time.Now().UnixNano() / 1000000
+	go skynet(c, 0, 1000000, 10)
+	result := <-c
+	end := time.Now().UnixNano() / 1000000
+	fmt.Printf("Result: %d in %d ms.\n", result, end-start)
 }

7988e5ad716205be1e596ea93d8a10bd126d0662
Shaun Crampton <shaun@cantab.net>
2016-02-14 16:09:50 +0000

    Add Python version.

---
+++ python-gevent/skynet.py
@@ -0,0 +1,20 @@
+import gevent
+
+
+def skynet(num, size, div):
+    if size == 1:
+        return num
+    else:
+        children = []
+        for i in xrange(div):
+            factor = size / div
+            sub_num = num + i * factor
+            children.append(gevent.spawn(skynet, sub_num, factor, div))
+        return sum((c.get() for c in children))
+
+
+if __name__ == "__main__":
+    greenlet = gevent.spawn(skynet, 0, 1000000, 10)
+    result = greenlet.get()
+    print(result)
+    assert result == 499999500000

fa563acad3d5d0eefe0d8617c7c10506c3450e4b
Chris Allen <cma@bitemyapp.com>
2016-02-14 15:21:57 -0600

    cleanup

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -3,9 +3,11 @@ module Main (main) where
 import Criterion.Main
 
 import qualified Chan
+import qualified MVar
 import qualified Parallel
 
 main :: IO ()
 main = defaultMain
-  [ bench "parallel" $ whnfIO Parallel.run
+  [ bench "parallel" $ whnfIO Parallel.run -- 51 ms
+  -- , bench "MVar" $ whnfIO MVar.run -- 2.2 s
   ]

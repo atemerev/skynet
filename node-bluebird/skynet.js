4de75d7395f02ed8dd947d396e44b206e0a0bf77
Alex Otenko <alex.otenko@menlosecurity.com>
2016-02-15 10:23:40 +0000

    less array resizing

--- node-bluebird/skynet.js
+++ node-bluebird/skynet.js
@@ -1,35 +1,38 @@
-var Promise = require('bluebird')
+'use strict'
+const Promise = require('bluebird')
+const div = 10
 
 function skynetAsync(num, size, div) {
   if (size == 1) {
     return num; //Promise.resolve(num);
   }
   else {
-    var tasks = [];
-    for (var i = 0; i < div; i++) {
-      var sub_num = num + i * (size / div);
-      var task = skynetAsync(sub_num, size / div, div);
-      tasks.push(task);
+    const tasks = new Array(div);
+    const sz = size/div;
+    for (let i = 0; i < div; i++) {
+      const sub_num = num + i * sz;
+      const task = skynetAsync(sub_num, sz, div);
+      tasks[i]=task;
     }
     return Promise.all(tasks).then(sum);
   }
 }
 
 function sum(values) {
-    var sum = 0;
-    for (var k = 0; k < values.length; ++k) {
-        sum += values[k]
-    }
-    return sum;
+  var sum = 0;
+  for (var k = 0; k < values.length; ++k) {
+    sum += values[k]
+  }
+  return sum;
 }
 
 console.time("regular")
-skynetAsync(0, 1000000, 10)
+skynetAsync(0, 1000000, div)
 .then(res => {
     console.log(res)
     console.timeEnd("regular")
     console.time("warmed-up")
-    return skynetAsync(0, 1000000, 10)
+    return skynetAsync(0, 1000000, div)
 }).then(res => {
     console.log(res)
     console.timeEnd("warmed-up")

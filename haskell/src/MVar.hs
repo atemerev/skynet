3eed1dea369538c65fa957566b11bad80c11bd84
Curry Haskell <ghc.make@gmail.com>
2016-03-08 16:15:15 +0300

    the MVar variant controlled by level rather than size
    similar to the Parallel variant
    
    Signed-off-by: Curry Haskell <ghc.make@gmail.com>

--- haskell/src/MVar.hs
+++ haskell/src/MVar.hs
@@ -7,29 +7,34 @@ import Control.Concurrent.MVar (MVar, newEmptyMVar, putMVar, takeMVar)
 import Control.Monad (forM, foldM)
 import Data.Time.Clock (getCurrentTime, diffUTCTime)
 
+children = 10
+
 plus1 :: Int -> MVar Int -> IO Int
-plus1 !n rc = do
+plus1 !n !rc = do
     n' <- takeMVar rc
     return $ n + n' 
 
-forkSkynet :: Int -> Int -> Int -> IO (MVar Int)
-forkSkynet !num !size !div = do
+forkSkynet :: Int -> Int -> IO (MVar Int)
+forkSkynet !lvl !num = do
             rc <- newEmptyMVar
-            _ <- forkIO $ skynet rc num size div
+            _  <- forkIO $ skynet rc lvl num
             return rc
 
-skynet :: MVar Int -> Int -> Int -> Int -> IO ()
-skynet c !num     1 div = putMVar c num
-skynet c !num !size div = let sizeDiv = size `quot` div in do
-        rcs <- forM [0..div-1] (\i -> let subNum  = num + i * sizeDiv in
-            forkSkynet subNum sizeDiv div)
+skynet :: MVar Int -> Int -> Int -> IO ()
+skynet c    0 !num = putMVar c num
+skynet c !lvl !num = let
+    !numFirst = num      * children
+    !numLast  = numFirst + children - 1
+    !lvl1     = lvl - 1
+    in do
+        rcs <- forM [numFirst..numLast] $ forkSkynet lvl1
         result <- foldM plus1 0 rcs
         putMVar c result
 
 run :: IO ()
 run = do
     start  <- getCurrentTime
-    c      <- forkSkynet 0 1000000 10
+    c      <- forkSkynet 6 0
     result <- takeMVar c
     end    <- getCurrentTime
     putStrLn $ concat [ "Result: "

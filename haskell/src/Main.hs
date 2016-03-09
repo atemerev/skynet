3ef7b4fe9aca9b0a5e30af811525bddc05a561b5
Curry Haskell <ghc.make@gmail.com>
2016-03-09 23:51:16 +0300

    added measuring of memory usage
    Signed-off-by: Curry Haskell <ghc.make@gmail.com>

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,18 +1,56 @@
 module Main (main) where
 
-import Criterion.Main
+import Criterion.Main     (defaultMain, bench, whnfIO)
+import GHC.Stats          (getGCStats, getGCStatsEnabled)
 
-import qualified Chan
-import qualified MVar
-import qualified Parallel
-import qualified TBQueue
-import qualified Unagi
+import qualified Chan     (run)
+import qualified MVar     (run)
+import qualified Parallel (run)
+import qualified TBQueue  (run)
+import qualified Unagi    (run)
 
-main :: IO ()
-main = defaultMain  -- i7-4930K CPU @ 3.40GHz, six cores, +RTS -N6
-    [ bench "parallel" $ whnfIO Parallel.run -- 0.017 s
+measureTime :: IO ()
+measureTime  = defaultMain  -- i7-4930K CPU @ 3.40GHz, six cores, +RTS -N6
+    [ bench "Parallel" $ whnfIO Parallel.run -- 0.017 s
     , bench "MVar"     $ whnfIO     MVar.run -- 1.7 s
     , bench "Chan"     $ whnfIO     Chan.run -- 1.8 s
     , bench "TBQueue"  $ whnfIO  TBQueue.run -- 6.8 s
     , bench "Unagi"    $ whnfIO    Unagi.run -- 15  s
     ]
+
+measureMemory :: IO ()
+measureMemory = do
+    putStrLn "on start"
+    stats0 <- getGCStats
+    putStrLn $ show stats0
+    putStrLn ""
+    putStrLn "Parallel"
+    Parallel.run
+    stats1 <- getGCStats
+    putStrLn $ show stats1
+    putStrLn ""
+    putStrLn "MVar"
+    MVar.run
+    stats2 <- getGCStats
+    putStrLn $ show stats2
+    putStrLn ""
+    putStrLn "Chan"
+    Chan.run
+    stats3 <- getGCStats
+    putStrLn $ show stats3
+    putStrLn ""
+    putStrLn "TBQueue"
+    TBQueue.run
+    stats4 <- getGCStats
+    putStrLn $ show stats4
+    putStrLn ""
+    putStrLn "Unagi"
+    Unagi.run
+    stats5 <- getGCStats
+    putStrLn $ show stats5
+
+main :: IO ()
+main = do
+    memory <- getGCStatsEnabled -- true if +RTS -T
+    let measure = if memory then measureMemory else measureTime
+    measure
